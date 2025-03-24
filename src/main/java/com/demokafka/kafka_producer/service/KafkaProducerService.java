9d7da46bd8c978923a651670e684e3441f82582c
keskumar5 <keshavkumar.er97@gmail.com>
2025-03-24 10:54:43 +0530

    update

--- src/main/java/com/demokafka/kafka_producer/service/KafkaProducerService.java
+++ src/main/java/com/demokafka/kafka_producer/service/KafkaProducerService.java
@@ -1,17 +1,37 @@
 package com.demokafka.kafka_producer.service;
 
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @Service
 public class KafkaProducerService {
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final WebClient webClient;
 
-    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
+    @Value("${kafka.bridge.url}")
+    private String kafkaBridgeUrl;
+
+    @Value("${kafka.topic}")
+    private String topic;
+
+    public KafkaProducerService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
     }
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("my-topic", message);
+        String url = kafkaBridgeUrl + "/topics/" + topic;
+
+        webClient.post()
+                .uri(url)
+                .bodyValue(Map.of(
+                        "key", "my-key",
+                        "value", message
+                ))
+                .retrieve()
+                .bodyToMono(String.class)
+                .subscribe(response -> System.out.println("Message Sent: " + response));
     }
 }
+
